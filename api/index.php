2cda605de1afecdff2ee022b32d6e7db8cd71cdf
Irza-12 <fhahlefi123@gmail.com>
2026-01-22 11:23:39 +0700

    Include SQLite DB for Vercel Demo

--- api/index.php
+++ api/index.php
@@ -1,3 +1,18 @@
 <?php
 
+
+// Vercel /tmp setup for SQLite
+$dbPath = '/tmp/database.sqlite';
+$source = __DIR__ . '/../database/database.sqlite';
+
+if (!file_exists($dbPath) && file_exists($source)) {
+    copy($source, $dbPath);
+}
+
+// Force SQLite connection for Vercel
+$_ENV['DB_CONNECTION'] = 'sqlite';
+$_ENV['DB_DATABASE'] = $dbPath;
+putenv("DB_CONNECTION=sqlite");
+putenv("DB_DATABASE={$dbPath}");
+
 require __DIR__ . '/../public/index.php';
